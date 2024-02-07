695177fb3014c0aecaba636ec1d2ec1f935c49e8
ramo-dev <annuar.dev@gmail.com>
2024-02-07 15:47:07 +0300

    navbar

--- vite-project1/src/components/Navbar.jsx
+++ vite-project1/src/components/Navbar.jsx
@@ -1,23 +1,40 @@
 import './NavbarStyles.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { menuItems } from './MenuItems';
 import { Link } from 'react-router-dom';
-
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
+
+    const [clicked, setClicked] = useState(false);
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
+
     return ( 
         <nav className='NavbarItems'>
-            <img src='../assets/GirrafeWithAcamera-logo.png' className='Navbar-logo'></img>
-            <ul className='Nav-menu'>
+            <a href='/' className='Navbar-logo'>
+                <img src={logo} alt="Girrafe With a Camera"></img>
+                <span className='Navbar-name'>Hazel</span>
+            </a>
+
+            <div className='menu-icons' onClick={handleClick}>
+                <i className={clicked ? "fas fa-times" : "fas fa-bars"} ></i>
+            </div>
+
+
+            <ul className={clicked ? "nav-menu active" : "nav-menu"}>
                 {menuItems.map((item, index)=>{
                      return(
                         <li key={index}>
-                            <a href={item.url}>
+                            <a href={item.url} className={item.cName}>
                                 <i className={item.icon}></i>{item.title}
                             </a>
                         </li>
                      )
                 })}
+                <button>Sign Up</button>
             </ul>
         </nav>
     );
