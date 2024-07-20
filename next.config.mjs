eb1c83916c5a825cd680f7b649039f53cb773940
ramo-dev <annuar.dev@gmail.com>
2024-07-21 02:46:55 +0300

    Added readme and repo files

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   images: {
+    domains: ['images.unsplash.com', "https://www.behance.net", "firebasestorage.googleapis.com"],
+  },
+};
+
+export default nextConfig;
